42e7608f5e86691ab899bd2c800548a979840fa8
Eriks Licis <71380662+licis124@users.noreply.github.com>
2020-10-29 18:27:55 +0200

    Create for_factorial.c

---
+++ PU3/for_factorial.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void) {
+  char a;
+  printf("Ievadi burtu ar kuru datu tipu tu gribi strādāt : int(i), char(c), long(l) : ");
+  scanf("%c",&a);
+  if(a == 'i'){
+       int c, n, f = 1;
+  printf("Ievadi skaitli no kuras gribi dabūt faktoriālu : \n");
+  scanf("%d", &n);
+  for (c = 1; c <= n; c++)
+    f = f * c;
+ if(f == 0){
+  printf("kļūda\n");
+  }else{
+    printf("Faktorials no %d = %d\n", n, f);
+  }
+  }
+
+  if(a == 'c'){
+      char c, n, f = 1;
+  printf("Ievadi skaitli no kuras gribi dabūt faktoriālu : \n");
+  scanf("%d", &n);
+  for (c = 1; c <= n; c++)
+    f = f * c;
+ if(f == 0){
+  printf("kļūda\n");
+  }else{
+    printf("Faktorials no %d = %d\n", n, f);
+  }
+  }
+ 
+
+  if(a == 'l'){
+     long c, n, f = 1;
+  printf("Ievadi skaitli no kuras gribi dabūt faktoriālu : \n");
+  scanf("%ld", &n);
+  for (c = 1; c <= n; c++)
+    f = f * c;
+ if(f == 0){
+  printf("kļūda\n");
+  }else{
+    printf("Faktorials no %ld = %ld\n", n, f);
+  }
+  }
+  return 0;
+}
