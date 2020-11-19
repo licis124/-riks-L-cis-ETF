2fc218e97f3252c71c4e74a9c7505ff85b6da04c
Eriks Licis <71380662+licis124@users.noreply.github.com>
2020-11-19 09:44:45 +0200

    Update Programming_Microcontroler.c

--- Programming_Microcontroler.c
+++ Programming_Microcontroler.c
@@ -43,3 +43,37 @@ volatile unsigned int n;
     ;
   }
   }
+
+
+
+//LEKCIJA 19.11.2020
+#include <msp430.h>
+
+int main()
+
+{
+
+  WDTCTL = WDTPW + WDTHOLD;
+
+  volatile unsigned int j=0;
+
+  P1DIR |= 0b10010001; 
+
+  P2DIR &= ~0b00001000;
+
+ for( ; ; )
+
+ {
+
+   //P1OUT ^= 0b00010001;
+   for(j=30000;j>1;j--);//aizture
+   if ((P2IN&0b00001000) == 0b00001000)
+   {
+     P1OUT |= 0b10000000;//ieslēgt 7.led
+   }
+   else
+   {
+     P1OUT &= ~0b10000000;
+   }
+ }
+}
